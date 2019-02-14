c1c650fe8f17667f6b2fc4f3345628525d62733f
suvanshchawla <suvanshchawla83@gmail.com>
2019-02-14 20:03:42 +0530

    complete code for Coinchange

--- coinchange.cpp
+++ coinchange.cpp
@@ -1,11 +1,45 @@
 #include<iostream>
+#include<algorithm>
  using namespace std;
 
  int main(int argc, char const *argv[])
  {
-     int n;
+     int n = 0, sum = 0;
      cin>>n;
-     return 0;
+     int den[n];
+     cout<<"Enter the denominations"<<endl;
+     for(int i = 0; i< n; i++){
+        cin>>den[i];
+     }
+     cout<<"Enter the sum: \t";
+     cin>>sum;
+    /* for(int i = 0; i< n; i++){
+        cout<<den[i]<<"\t"<<endl;
+    } */ 
+
+    int coins[n][sum+1];
+    std::fill((int*)coins,(int*)coins+sizeof(coins)/sizeof(int),0);
+
+    for(int i = 0; i< n; i++){
+        coins[i][0] = 1;
+    }
+
+    int x1 = 0, x2 = 0;
+    for(int i = 0; i<n; i++){
+        for(int j = 1; j<sum+1;j++){
+            if(den[i]>j){
+                coins[i][j] = 0;
+            }
+            else{
+                x1 = (i>0)?coins[i-1][j]:0;
+                x2 = coins[i][j-den[i]];   
+            }
+            coins[i][j] = x1+x2;    
+        }
+    }
+    
+    cout<<coins[n-1][sum];
+    return 0;
  }
  /*
     So basically what we do is:
